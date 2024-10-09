5d8526b7c67aa9e8f8b46c84389e0b6745dc82a3
mrashed <mrashed@nadeer.com.sa>
2024-10-09 15:53:59 +0300

    feat: add AppStrings class

--- lib/src/translation/localization.dart
+++ lib/src/translation/localization.dart
@@ -1,37 +1,46 @@
+import 'dart:convert';
 import 'dart:developer';
 
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-class Localization {
+class MainAppStrings {
   Translations? _translations, _fallbackTranslations;
   late Locale _locale;
 
-  bool _useFallbackTranslationsForEmptyResources = false;
+  MainAppStrings();
 
-  Localization();
+  static MainAppStrings? _instance;
+  static MainAppStrings get instance =>
+      _instance ?? (_instance = MainAppStrings());
 
-  static Localization? _instance;
-  static Localization get instance => _instance ?? (_instance = Localization());
+  static MainAppStrings? of(BuildContext context) =>
+      Localizations.of<MainAppStrings>(context, MainAppStrings);
 
-  static Localization? of(BuildContext context) =>
-      Localizations.of<Localization>(context, Localization);
+  static const LocalizationsDelegate<MainAppStrings> delegate =
+      _MainAppStringsDelegate();
 
-  static const LocalizationsDelegate<Localization> delegate =
-      _MainAppLocalizationsDelegate();
+  static Future<Map<String, dynamic>> loadResources(Locale locale) async {
+    String jsonString = await rootBundle.loadString(
+        'packages/main_app_strings/src/translation/${locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
 
-  static bool load(
+    final translations = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+    return translations;
+  }
+
+  static Future<bool> load(
     Locale locale, {
-    Translations? translations,
     Translations? fallbackTranslations,
     bool useFallbackTranslationsForEmptyResources = false,
-    bool ignorePluralRules = true,
-  }) {
+  }) async {
     instance._locale = locale;
-    instance._translations = translations;
+    instance._translations =
+        Translations(await loadResources(instance._locale));
     instance._fallbackTranslations = fallbackTranslations;
-    instance._useFallbackTranslationsForEmptyResources =
-        useFallbackTranslationsForEmptyResources;
-    return translations == null ? false : true;
+    return instance._translations == null ? false : true;
   }
 
   String tr(String key) {
@@ -40,8 +49,7 @@ class Localization {
 
   String _resolve(String key, {bool logging = true, bool fallback = true}) {
     var resource = _translations?.get(key);
-    if (resource == null ||
-        (_useFallbackTranslationsForEmptyResources && resource.isEmpty)) {
+    if (resource == null || (resource.isEmpty)) {
       if (logging) {
         log('Localization key [$key] not found');
       }
@@ -49,8 +57,7 @@ class Localization {
         return key;
       } else {
         resource = _fallbackTranslations?.get(key);
-        if (resource == null ||
-            (_useFallbackTranslationsForEmptyResources && resource.isEmpty)) {
+        if (resource == null || (resource.isEmpty)) {
           if (logging) {
             log('Fallback localization key [$key] not found');
           }
@@ -59,10 +66,6 @@ class Localization {
       }
     }
     return resource;
-  }
-
-  bool exists(String key) {
-    return _translations?.get(key) != null;
   }
 }
 
@@ -75,33 +78,8 @@ class Translations {
   }
 }
 
-class MainAppMessage {
-  final String? key;
-  String? message;
-
-  MainAppMessage({
-    this.key,
-    this.message,
-  });
-
-  /// Localizes the validation message by looking up the key in the localizations.
-  ///
-  /// If the key is found, the localized message will be returned. If no localization is found,
-  /// the default message is used instead.
-  ///
-  /// - Parameters:
-  ///   - [context]: The current BuildContext used for localization.
-  ///
-  /// - Returns: The localized message or the default message if localization is not found.
-  String? localize(BuildContext context) {
-    message = Localization().of(context)?.translate(key) ?? message;
-    return message;
-  }
-}
-
-class _MainAppLocalizationsDelegate
-    extends LocalizationsDelegate<Localization> {
-  const _MainAppLocalizationsDelegate();
+class _MainAppStringsDelegate extends LocalizationsDelegate<MainAppStrings> {
+  const _MainAppStringsDelegate();
 
   @override
   bool isSupported(Locale locale) {
@@ -109,12 +87,11 @@ class _MainAppLocalizationsDelegate
   }
 
   @override
-  Future<Localization> load(Locale locale) async {
-    Localization localizations = Localization(locale);
-    await localizations.load();
-    return localizations;
+  Future<MainAppStrings> load(Locale locale) async {
+    await MainAppStrings.load(locale);
+    return MainAppStrings.instance;
   }
 
   @override
-  bool shouldReload(_MainAppLocalizationsDelegate old) => false;
+  bool shouldReload(_MainAppStringsDelegate old) => false;
 }
